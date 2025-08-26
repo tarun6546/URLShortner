d7fad14598f9fbe47611638b57c9a0a3e3720f7e
tarun6546 <tarunvarshney2112@gmail.com>
2025-08-26 09:14:41 +0530

    create url shortner

--- controllers/url.js
+++ controllers/url.js
@@ -1,4 +1,16 @@
+const {nanoid} = require('nanoid');
+
 async function handleGenerateShortUrl(req, res) {
-    // Logic to generate a short URL
-    res.send('Short URL generated');
+    const body = req.body;
+    if(!body.url) return res.status(400).json({error: "URL is required"});
+    const shortId = nanoid(8);
+    await URL.createObjectURL({
+        shortId: shortId,
+        redirectURL: body.url,
+        visitHistory: [],
+
+    })
+    return res.json({id: shortId});
+
 }
+moduke.exports = {handleGenerateShortUrl};
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+
+const urlRoute = require('./routes/url');
+
 const app = express();
 const PORT = 8001;
+
+app.use("/url",urlRoute);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
